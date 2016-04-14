d8f9eb20bec1f9d3aff559db61c15be1014c93de
shubh28 <shubh_jain94@yahoo.co.in>
2016-04-14 16:07:35 +0530

    Data body changed

--- src/components/dataComponents/CompanyDataBody.jsx
+++ src/components/dataComponents/CompanyDataBody.jsx
@@ -3,6 +3,50 @@ var React = require('react');
 var CompanyDataBody = React.createClass({
 	render: function () {
 		return (		
+
+					<div className="collapsible-header"><h5><b>{this.props.dataitem.name}</b></h5></div>
+				      <div className="collapsible-body">
+					      <div className="row">
+						    <div className="card-panel teal  white-text ">
+							  	<a className="waves-effect waves-light btn blue right" >Edit</a>
+								<div className="clear-fix"></div>
+
+								
+								<div>
+									<dl>
+						      			<dt>Address</dt>
+						      			<dd>{this.props.dataitem.address}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>City</dt>
+						      			<dd>{this.props.dataitem.city}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Email</dt>
+						      			<dd>{this.props.dataitem.email}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Contact Number</dt>
+						      			<dd>{this.props.dataitem.contactNumber}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Website</dt>
+						      			<dd>{this.props.dataitem.website}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Contractor</dt>
+						      			<dd>{this.props.dataitem.contractor}</dd>
+						      		</dl>
+						      	</div>
+
+						      	<this.props.bodyComponent dataitem={this.props.dataitem}/>
+						    </div>
+							<a className="waves-effect waves-light btn blue right" >Check Parking Lots</a>
+							<div className="clear-fix"></div>		
+					    </div>
+				     </div>
+
+
 						<div>
 							<dl>
 				      			<dt>Address</dt>
--- src/components/dataComponents/ParkingDataBody.jsx
+++ src/components/dataComponents/ParkingDataBody.jsx
@@ -3,44 +3,63 @@ var React = require('react');
 var ParkingDataBody = React.createClass({
 	render: function () {
 		return (		
-						<div>
-							<dl>
-				      			<dt>Address</dt>
-				      			<dd>{this.props.dataitem.address}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>City</dt>
-				      			<dd>{this.props.dataitem.city}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>Contact Number</dt>
-				      			<dd>{this.props.dataitem.contactNumber}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>Category</dt>
-				      			<dd>{this.props.dataitem.category}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>Landmark</dt>
-				      			<dd>{this.props.dataitem.landmark}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>CreatedBy</dt>
-				      			<dd>{this.props.dataitem.createdBy}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>UpdatedBy</dt>
-				      			<dd>{this.props.dataitem.updatedBy}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>CreatedAt</dt>
-				      			<dd>{this.props.dataitem.createdAt}</dd>
-				      		</dl>
-				      		<dl>
-				      			<dt>UpdatedAt</dt>
-				      			<dd>{this.props.dataitem.updatedAt}</dd>
-				      		</dl>
-				      	</div>	
+
+
+			      <div className="collapsible-header"><h5><b>{this.props.dataitem.name}</b></h5></div>
+			      <div className="collapsible-body">
+				      <div className="row">
+					      	<div className="card-panel teal  white-text ">
+						      	<a className="waves-effect waves-light btn blue right" >Edit</a>
+								<div className="clear-fix"></div>
+
+								<div>
+									<dl>
+						      			<dt>Address</dt>
+						      			<dd>{this.props.dataitem.address}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>City</dt>
+						      			<dd>{this.props.dataitem.city}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Contact Number</dt>
+						      			<dd>{this.props.dataitem.contactNumber}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Category</dt>
+						      			<dd>{this.props.dataitem.category}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>Landmark</dt>
+						      			<dd>{this.props.dataitem.landmark}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>CreatedBy</dt>
+						      			<dd>{this.props.dataitem.createdBy}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>UpdatedBy</dt>
+						      			<dd>{this.props.dataitem.updatedBy}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>CreatedAt</dt>
+						      			<dd>{this.props.dataitem.createdAt}</dd>
+						      		</dl>
+						      		<dl>
+						      			<dt>UpdatedAt</dt>
+						      			<dd>{this.props.dataitem.updatedAt}</dd>
+						      		</dl>
+						      	</div>
+
+					      		<this.props.bodyComponent dataitem={this.props.dataitem}/>
+					      	</div>
+						    <a className="waves-effect waves-light btn blue right" >Check Parking Lots</a>
+							<div className="clear-fix"></div>		
+				      </div>
+			      </div>
+
+
+	
 			);
 	}
 });
